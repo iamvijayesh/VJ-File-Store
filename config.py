6b2cb3b521ecea9e85a5bf2b6d13e937d90787a0
iamvijayesh <vijayeshvishal@gmail.com>
2024-12-27 11:44:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "29292134"))
 API_HASH = environ.get("API_HASH", "de07f7315d14befd6d8d434b29dfcea7")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7532418446:AAGjPomtYHLi00gAt0R7aTLOOLcWCa1JRAA")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://envs.sh/odI.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7613080889').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "MBAhelpallbot") # without @
 PORT = environ.get("PORT", "8080")

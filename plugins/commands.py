ba70b3608d3b3cbd5efc3f9b49297d84d4097c75
iamvijayesh <vijayeshvishal@gmail.com>
2024-12-29 15:54:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,7 +13,7 @@ from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
 from pyrogram.errors import ChatAdminRequired, FloodWait
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import *
 
 from utils import verify_user, check_token, check_verification, get_token
 from config import *

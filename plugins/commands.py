61a832a9cffe4eafee435388f4f607e515beb4e9
iamvijayesh <vijayeshvishal@gmail.com>
2024-12-29 23:41:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -96,19 +96,6 @@ async def start(client, message):
             reply_markup=reply_markup
         )
         return
-if AUTH_CHANNEL and not await is_subscribed(client, message):
-        try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
-        except ChatAdminRequired:
-            logger.error("Make sure Bot is admin in Forcesub channel")
-            return
-        btn = [
-            [
-                InlineKeyboardButton(
-                    "join updates", url=invite_link.invite_link
-                )
-            ]
-        ]
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01

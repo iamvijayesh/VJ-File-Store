74ae529cc30cd7570d9e3c5ccc2bd7cbf7b19e0f
iamvijayesh <vijayeshvishal@gmail.com>
2025-01-04 22:13:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}</b>""" 
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @HelpXAT_bot{file_name}</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
